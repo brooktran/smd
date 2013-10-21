81a63b02be0b0037bcd2d9bbc6fbf0da8b592518
chenzhw <ahiwoo@163.com>
2013-10-21 12:55:24 +0800

    adds products/article views

---
+++ themes/classic/views/back/product/admin.php
@@ -0,0 +1,59 @@
+<?php
+/* @var $this ProductController */
+/* @var $model Product */
+
+$this->breadcrumbs=array(
+	'Products'=>array('index'),
+	'Manage',
+);
+
+$this->menu=array(
+	array('label'=>'List Product', 'url'=>array('index')),
+	array('label'=>'Create Product', 'url'=>array('create')),
+);
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$('#product-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
+?>
+
+
+
+<h1>产品管理</h1>
+
+<p>
+可以使用 (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+或者 <b>=</b>) 进行过滤.
+</p>
+
+<?php echo CHtml::link('高级搜索','#',array('class'=>'search-button')); ?>
+<div class="search-form" style="display:none">
+<?php $this->renderPartial('_search',array(
+	'model'=>$model,
+)); ?>
+</div><!-- search-form -->
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'product-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+		'id',
+		'fdAreaID',
+		'fdContentID',
+		'fdDomainID',
+		'fdColumn',
+		array(
+			'class'=>'CButtonColumn',
+		),
+	),
+)); ?>
