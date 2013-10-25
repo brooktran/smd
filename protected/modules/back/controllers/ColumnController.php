9989480809b445c311728fdaefc6a6ed1d81d636
laojiu <407898830@qq.com>
2013-10-25 16:27:08 +0800

    分类column控制器

--- protected/modules/back/controllers/ColumnController.php
+++ protected/modules/back/controllers/ColumnController.php
@@ -10,6 +10,23 @@
 class ColumnController extends Controller{
 
     public function actionIndex(){
+        $column= ColumnService::factory()->getAllCategory();
+
+        $this->render('index',array('column'=>$column));
+    }
+
+    /**
+     * 删除分类
+     */
+    public function actionDelete(){
+        $id = $_GET['id'];
+        if(!$id){
+            $this->redirect($this->createUrl('/back/column'));
+        }
+        $column= ColumnService::factory()->deleteByCateId($id);
+        if($column){
+            $this->redirect($this->createUrl('/back/column'));
+        }
     }
 
     /**
@@ -25,15 +42,47 @@ class ColumnController extends Controller{
             $attributes['fdName'] = $_POST['CategoryForm']['name'];
             $attributes['fdDescription'] = $_POST['CategoryForm']['introduce'];
             $attributes['fdTypeID'] = $_POST['CategoryForm']['flag']==11 ? 11 : 12;
-            $attributes['fdParentID'] = isset($_POST['CategoryForm']['pid']) ? $_POST['pid'] : 0;
+            $attributes['fdParentID'] = isset($_POST['CategoryForm']['fdParentID']) ? $_POST['CategoryForm']['fdParentID'] : 0;
 
             $column = ColumnService::factory()->saveColumn($attributes);
 
             if($column){
-               // $this->redirect(array('view','id'=>$model->id));
+                $this->redirect($this->createUrl('/back/column'));
             }
         }
+        $category = ColumnService::factory()->getAllCategory();
 
-        $this->render('create',array('model'=>$model,'flag'=>$flag));
+        $this->render('create',array('model'=>$model,'flag'=>$flag,'cates'=>$category));
     }
+
+    public function actionUpdate(){
+        $model = new CategoryForm();
+        $attributes = array();
+        $flag =  $_GET['tid'];//分类type
+
+        if(isset($_POST['CategoryForm'])){
+            $attributes['id'] = $_POST['CategoryForm']['id'];
+            $attributes['fdName'] = $_POST['CategoryForm']['name'];
+            $attributes['fdDescription'] = $_POST['CategoryForm']['introduce'];
+            $attributes['fdTypeID'] = $_POST['CategoryForm']['flag']==11 ? 11 : 12;
+            $attributes['fdParentID'] = isset($_POST['CategoryForm']['fdParentID']) ? $_POST['CategoryForm']['fdParentID'] : 0;
+
+            $column = ColumnService::factory()->saveColumn($attributes);
+
+            if($column){
+                $this->redirect($this->createUrl('/back/column'));
+            }
+        }
+        $id = $_GET['id'];
+        $category = ColumnService::factory()->getCategoryByID($id);
+        $categorys = ColumnService::factory()->getAllCategory();
+        $this->render('update',array(
+            'model'=>$model,
+            'id'=>$id,
+            'flag'=>$flag,
+            'cate'=>$category,
+            'cates'=>$categorys
+        ));
+    }
+
 }
