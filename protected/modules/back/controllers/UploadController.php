1444f049d623893a43c3723fc3aa4785fe89591f
laojiu <407898830@qq.com>
2013-11-17 21:54:20 +0800

    UploadController.php

---
+++ protected/modules/back/controllers/UploadController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: user
+ * Date: 13-11-17
+ * Time: 上午2:14
+ * To change this template use File | Settings | File Templates.
+ */
+class UploadController extends Controller{
+
+    public  function actionIndex(){
+        $this->render('index');
+    }
+
+    public function actionUpload(){
+        $file = XUpload::upload( $_FILES['imgFile'] ,$params = array( 'thumb'=>true));
+
+
+
+   }
+
+}
